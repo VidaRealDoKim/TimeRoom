1f94ea68b8e850834a53afb0ebcc905c0768b91e
David Luis Kim <76842136+VidaRealDoKim@users.noreply.github.com>
2025-10-02 16:58:46 -0300

    Update reservar_salas.dart

--- apk/lib/user/reserva/reservar_salas.dart
+++ apk/lib/user/reserva/reservar_salas.dart
@@ -1,4 +1,4 @@
-import 'package:apk/user/reserva/pages/detalhes_sala.dart';
+import 'package:apk/user/home/reservar/detalhes_sala.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
